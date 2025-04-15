b84b4c801f843e194794f0ca5637e1e1eb807184
nidal <nidal@fit-x.app@outlook.com>
2025-04-15 15:44:51 +0300

    remove ununsed libraries & files

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import TicTacToe from '../../components/TicTacToe';
 
 export default function HomeScreen() {
   return (
+    <View style={styles.container}>
       <TicTacToe />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // Styles removed as they're not needed for this component anymore
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
